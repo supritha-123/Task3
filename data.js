cc965a5f3629ea763c104dd900347edc4b51695b
supritha-123 <66063617+supritha-123@users.noreply.github.com>
2020-06-22 21:48:32 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,81 @@
+{
+	"profiles":[
+		{
+			"basics":{
+				"name":"sai supritha",
+				"role":"Student",
+				"email":"oohagnanamadhuri@gmail.com",
+				"mobile":"+91 6305815640",
+				"gender":"female",
+				"address":"vijayawada,Andhra pradesh"
+
+			},
+			"summery":[
+				"I am Fresher in computer science engineering",
+                "But i having knowledge in some technologies like HTML5,CSS3,and Javascript",
+                "Having knowledge on Python, C++,React",
+                "Used different kinds of frameworks for backend i.e Codeigniter,Django and Express",
+                "Used databases like mySql,oracle and firebase"
+
+			],
+			"education":[
+				{
+                    "degree":"B-Tech (Computer Science and Engineering)",
+                    "institute":"Lakireddy balireddy College of Engineering",
+                    "result":"96%"
+				},
+				{
+                    "degree":"Intermediate in MPC",
+                    "institute":"Sri Chaitanya Junior College",
+                    "result":"96%"
+				},
+				{
+                    "degree":"S.S.C",
+                    "institute":"Amali e.m high school",
+                    "result":"95%"
+				}
+
+
+			]
+		},
+	    {
+	    	"basics":{
+				"name":"sriharsha",
+				"role":"Student",
+				"email":"sriharsha01@gmail.com",
+				"mobile":"+8519838047",
+				"gender":"female",
+				"address":"vijayawada,Andhra pradesh"
+
+			},
+			"summery":[
+				"I am Fresher in computer science engineering",
+                "But i having knowledge in some technologies like HTML5,CSS3,and Javascript",
+                "Having knowledge on Python, C++,React",
+                "Used different kinds of frameworks for backend i.e ,Django and Express",
+                "Used databases like mySql,oracle"
+
+			],
+			"education":[
+				{
+                    "degree":"B-Tech (Computer Science and Engineering)",
+                    "institute":"Lakireddy balireddy College of Engineering",
+                    "result":"88%"
+				},
+				{
+                    "degree":"Intermediate in MPC",
+                    "institute":"Sri Chaitanya Junior College",
+                    "result":"95%"
+				},
+				{
+                    "degree":"S.S.C",
+                    "institute":"Amali e.m high school",
+                    "result":"90%"
+				}
+
+
+			]
+	    }
+		
+	]
+}
